531c3ec81d706255f73e51c3cb35789568ec2e53
Dede Kurniawan <dekur98@outlook.com>
2021-02-05 21:43:58 +0700

    initial commit

---
+++ quiz/data.js
@@ -0,0 +1,28 @@
+const data = [
+    [
+        "ZORRO ARK",
+        [
+            "YOUNG LANGUAGE",
+            ["YOUTH KICK",16],
+            "ZHIVAGO CORE",
+            5,
+            "ZOOLANDER FICTION",
+        ]
+    ],
+    [21,[22,["BEVERLY OUTLAW"],25,[26,27], "BERETS AgEnt",]],
+    [
+        "BETRAYED REAR",
+        ,
+        [
+            "bIkini BORROWERS",
+            "YENTL IDAHO",
+            31,50,
+        ]
+    ],
+    [3,12,[14],19],
+    [
+        ["ACADEMY DINOSAUR",[8,9,10],["ACE goldfinger"], ["ADAPTATION HOLES", [ "AFFAIR PREJUDICE",["BENEATH RUSH"]]]],
+    ],
+]
+
+module.exports = data
